df045ee9ea98a5d1c1fbf7eb2101e6542989afb7
Nick Sheward <nickshwrd@gmail.com>
2023-04-17 15:00:25 +0100

    Created insert function

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
 import mergeSort from './mergeSort.js'; // Importing mergeSort algorithm from Recursion project
 
-const NodeFactory = (value, left = null, right = null) => ({
-  value,
+const NodeFactory = (data, left = null, right = null) => ({
+  data,
   left,
   right,
 });
 
 // Function to visualize the binary search tree in the console
-// const prettyPrint = (node, prefix = '', isLeft = true) => {
-//   if (node === null) {
-//     return;
-//   }
-//   if (node.right !== null) {
-//     prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
-//   }
-//   console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
-//   if (node.left !== null) {
-//     prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
-//   }
-// };
+const prettyPrint = (node, prefix = '', isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+  }
+  console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+  }
+};
 
-const root = null;
+let root = null;
 
 const buildTree = (array, start, end) => {
   // Base case to exit algorithm
@@ -45,10 +45,46 @@ const removeDuplicates = (array) => {
   return uniqueArray;
 };
 
+const insert = (value, node) => {
+  console.log(node);
+  // First check if tree has been initialised
+  if (node === null) {
+    root = NodeFactory(value);
+    // Check if a duplicate, if so throw error
+  }
+  if (node.data === value) {
+    console.log('Error, data already exists in tree');
+  }
+  // If the data at the current node is greater than the value to be inserted,
+  // go down the left subtree
+  else if (node.data > value) {
+    // If the current node has a left child, continue to search
+    if (node.left !== null) {
+      insert(value, node.left);
+      // else if there is no left child, insert the data
+    } else {
+      node.left = NodeFactory(value);
+    }
+    // Else the data at the current node is less than the value to be inserted
+  } else {
+    // if the current node has a right child, continue to search
+    if (node.right !== null) {
+      insert(value, node.right);
+    } else {
+      node.right = NodeFactory(value);
+    }
+  }
+};
+
 const myArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 const start = 0;
 const end = myArray.length - 1; // 2
 const sortedArray = mergeSort(myArray);
+const uniqueArray = removeDuplicates(sortedArray);
+root = buildTree(uniqueArray, start, end);
 
-console.log(sortedArray);
-console.log(removeDuplicates(sortedArray));
+console.log(root);
+
+insert(10, root);
+
+prettyPrint(root);
