ce94d68c9fc7cdae1e168944e0cf974edcb9b46c
Nick Sheward <nickshwrd@gmail.com>
2023-04-18 09:12:41 +0100

    Created deleteNode function

--- src/index.js
+++ src/index.js
@@ -45,7 +45,7 @@ const removeDuplicates = (array) => {
   return uniqueArray;
 };
 
-const insert = (value, node) => {
+const insertNode = (value, node) => {
   // First check if tree has been initialised
   if (node === null) {
     root = NodeFactory(value);
@@ -59,7 +59,7 @@ const insert = (value, node) => {
   else if (node.data > value) {
     // If the current node has a left child, continue to search
     if (node.left !== null) {
-      insert(value, node.left);
+      insertNode(value, node.left);
       // else if there is no left child, insert the data
     } else {
       node.left = NodeFactory(value);
@@ -68,36 +68,57 @@ const insert = (value, node) => {
   } else {
     // if the current node has a right child, continue to search
     if (node.right !== null) {
-      insert(value, node.right);
+      insertNode(value, node.right);
     } else {
       node.right = NodeFactory(value);
     }
   }
 };
 
+const findMin = (node) => {
+  // Find smallest value in the right subtree and return this value
+  // First look in the left subtree
+  if (node.left !== null) {
+    findMin(node.left);
+    // When the left subtree is empty, the function has found the smallest value
+  } else {
+    return node;
+  }
+};
+
 const deleteNode = (value, node) => {
-  // First check if tree has been initialised
+  // Base case if the tree is empty
   if (node === null) {
     console.log('Error, no data present in the tree');
     return null;
   }
-  // Recurse down the tree
-  if (node.data < value) {
-    deleteNode(value, node.left);
-  } else if (node.data > value) {
-    deleteNode(value, node.right);
-  } else {
-    // Then check if the node is a leaf, if so remove it from the tree
-    if (node.left === null && node.right === null) {
-      node.data = null;
-    }
-    if (node.left === null) {
+  // Recurse down the tree until the specified value is found, or exit via base case
+  if (node.data > value && node.left !== null) {
+    node.left = deleteNode(value, node.left);
+  } else if (node.data < value && node.right !== null) {
+    node.right = deleteNode(value, node.right);
+    // Function has found a value to delete
+  } else if (node.data === value) {
+    // If the target node has two children
+    if (node.left !== null && node.right !== null) {
+      // Search the right sub-tree for the smallest value to replace the node to be deleted
+      const minVal = findMin(node.right);
+      // Override the data to be deleted with the smallest value
+      node.data = minVal;
+      // Recursively delete the now duplicate node to the right
+      node.right = deleteNode(minVal, node.right);
+      // If the target node only has a left child
+    } else if (node.left !== null && node.right === null) {
+      return node.left;
+      // If the target node only has a right child
+    } else if (node.left === null && node.right !== null) {
       return node.right;
-    }
-    if (node.right === null) {
-      return root.left;
+      // Check if the node has no children and is therefore a leaf
+    } else if (node.left === null && node.right === null) {
+      return null;
     }
   }
+  return node;
 };
 
 const myArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
@@ -109,6 +130,11 @@ root = buildTree(uniqueArray, start, end);
 
 console.log(root);
 
-insert(10, root);
+insertNode(10, root); // correctly inserts 10 as a new node to the right of 9,
+// left of 6,345, 67 and 23
+
+prettyPrint(root);
+
+deleteNode(10, root);
 
 prettyPrint(root);
